ffe0634bdf6ed280afc64717aae649dcedd7934e
Jonathan Diamond <jdiamond@apple.com>
2015-09-02 00:09:58 -0700

    added basic logic for connecting to room

--- main.js
+++ main.js
@@ -9,12 +9,49 @@ var app = http.createServer(function (request, response) {
 	});
 }).listen(1337);
 
+
+var allClients = [];
+
 var io = require('socket.io').listen(app);
 
 io.sockets.on('connection', function(socket) {
-	socket.on('message_to_server', function(data) {
-	var escaped_message = data["message"];
-		console.log(escaped_message);
-		io.sockets.emit("message_to_client",{ message: escaped_message });
+	var user = {'socket':socket};
+	socket.on('join_room', function(data) {
+		if(user.room == null)
+		{
+			user.room = data["room"];
+			user.name = data["name"];
+			for (var i = 0; i < allClients.length; i++) {
+				if(allClients[i].room!=null && allClients[i].partner==null && allClients[i].room==user.room)
+				{
+					user.partner=allClients[i];
+					allClients[i].partner=user;
+					console.log("client "+user.name+" joined room "+user.room);
+					io.sockets.emit("room_joined",{"name":allClients[i].name});
+					user.partner.socket.emit("room_joined",{"name":user.name});
+				}
+			}
+
+			allClients.push(user);
+			if(	user.partner==null)
+			{
+				console.log("client "+user.name+" created room "+user.room);
+			}
+		}
 	});
+
+	socket.on('message_to_server', function(data) {
+		if(connected>0)
+		{
+			var escaped_message = data["message"];
+			console.log(escaped_message);
+			io.sockets.emit("message_to_client",{ message: escaped_message });
+		}
+	});
+
+	socket.on('disconnect', function () {
+
+      socket.emit('disconnected');
+
+  });
 });
